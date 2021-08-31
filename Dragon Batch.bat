dedd3d68e9a8692ef3fcfe68691a05b2b4641a09
Yaarian <dip.kumar020@gmail.com>
2021-08-31 19:36:17 +0600

    1st Commit, 31/8/2021

---
+++ Dragon Batch.bat
@@ -0,0 +1,56 @@
+@echo off
+color 0a
+title Dragon Batch
+echo DRAGON BATCH
+echo                                            `:+.                                              .+:`                       
+echo                                           :yNy.                                                `yNy:                     
+echo                                         :hNMs`                                                  `oMNd/                   
+echo                                       .hNMMy`                                                     sMMMh-                 
+echo                                      /NMMMd`                         `                            `dMMMN+                
+echo                                    `sNMMMM:                         `s`                            :MMMMMs`              
+echo                                   `yMMMMMm                        `:ymys.                           mMMMMMh`             
+echo                                  `yMMMMMMh                     `:ymNMMMmys/-..`                     yMMMMMMy`            
+echo                                  oMMMMMMMy                   -odNMMMMMMMMMmo:.`                     sMMMMMMMs            
+echo                                 :MMMMMMMMd                  -dMMMMMMMMMMMMMMs-`                     dMMMMMMMM/           
+echo                                `mMMMMMMMMMo`              -yNMMMMMMNmNMMMMMMMy-`                  `oMMMMMMMMMm`          
+echo                                oMMMMMMMMMMMd/`            hMMMMMNy+-`oMMMMMMMMNh-               `:hMMMMMMMMMMMs          
+echo                               `NMMMMMMMMMMMMMms:.   ```   ./+osyo   :mMMMMMMMh+hm/     ```  `.:sdMMMMMMMMMMMMMN.         
+echo                               +MMMMMMMMMMMMMMMMMmhhhddh:.`       `+yNMMMMMMMd.  -h` `.:yddhhdmMMMMMMMMMMMMMMMMMo         
+echo                               dMMMMMMMMMMMMMMMMMMMMMMMMMNdhyo+/::hMMMMMMMMMM+::/+yyhdNMMMMMMMMMMMMMMMMMMMMMMMMMm         
+echo                             .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-        
+echo                              /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+        
+echo                              sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy        
+echo                              yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh        
+echo                              yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh        
+echo                              yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh        
+echo                              yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh        
+echo                              sMMMMMMMMMMMMMMMNddMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdhNMMMMMMMMMMMMMMMy        
+echo                              /MMMMMMMMMMMMMm/` /MMMN+osdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdso+mMMM+ `/mMMMMMMMMMMMMM+        
+echo                              .MMMMMMMMMMMMm`   :/mMs    `:ohNMMMMMMMMMMMMMMMMMMMMNho:`    oMN::   `mMMMMMMMMMMMM-        
+echo                               dMMMMMMMMMMM:      dM:        `NMMMMMMMMMMMMMMMMMMN.        :Mm      -MMMMMMMMMMMm         
+echo                               +MMMMMMMMMMm       oM:        sMMNMMMMMMMMMMMMMMmMMy        -Mo       dMMMMMMMMMMo         
+echo                               `NMMMMMMMMMs       `d+        d/``hMMMMMMMMMMMMh``/m`       /m`       oMMMMMMMMMN`         
+echo                                oMMMMMMMMM+        .y        `   -yNMMMMMMMMNy-   .        y.        /MMMMMMMMMo          
+echo                                `mMMMMMMMM+         ``             .MMMMMMN+.             ``         /MMMMMMMMm`          
+echo                                 :NMMMMMMM+                        .MMMMMM/                          /MMMMMMMM:           
+echo                                  oMMMMMMMo                        .MMMMMm                           +MMMMMMMo            
+echo                                   sMMMMMMy                        `NMMMMh                           sMMMMMMy`            
+echo                                   `sMMMMMm                         oMMMMN`                          dMMMMMy`             
+echo                                    `oMMMMM:                        `smMMN.                         -MMMMMs`              
+echo                                      /NMMMh                          -NMMo                         yMMMN+                
+echo                                       .yMMM/                          +MMN.                       /MMMh-                 
+echo                                        `:hMN/                          dMMo                      :NMd/`                  
+echo                                          `:ymo`                        /MMd                    `omh:`                    
+echo                                            ./+-                       `NMN`                  .//.                       
+echo                                                                         hMM-                                             
+echo                                                                         +MM-                                             
+echo                                                                         :MM-                                             
+echo                                                                         .MM.                                             
+echo                                                                          NN                                              
+echo                                                                         `Nh                                              
+echo                                                                         -M/                                              
+echo                                                                        ssN.                                              
+echo                                                                       .MMh-                                              
+echo                                                                       oN+`                                               
+echo                                                                       +.  
+pause>nul                                              
---
+++ Welcome.bat
@@ -0,0 +1,42 @@
+@echo off
+REM @echo off for do not print the commands
+:: echo means print everyting on that line
+color A
+:: Color A is the Green Color                                                                                                    
+echo                                                                  :                                     -                                    
+echo                                                                 -d                                     -+                                   
+echo                                                                `d+                                      s/                                  
+echo                                                                sM.                                      :m`                                 
+echo                                                               `NM`                                      :M+                                 
+echo                                                               /MM:                                      yMh                                 
+echo                                                               +MMd`                                    /MMd                                 
+echo                                                               -MMMh`                                 `+NMMm                                 
+echo                                                                mMMMd/`                              `yMMMMy                                 
+echo                                                                +MMMMMmy+-                       ``:ydyyhdh-                                 
+echo                                                                 yMMMMyym+:.`                  ..-dd+NMMMN+                                  
+echo                                                                 `dMMMMy:-MMmy:             `/hNMh./NMMMMs                                   
+echo                                                                  `hMMMN/`smMMMh-         `oNMMNds-yMMMMs                                    
+echo                                                                   `sMMMN:  .+NMMy       :mMNs:` `yMMMMo                                     
+echo                                                                     -dMMMs-  .yNo:`    `hMo`   -mMMMN:                                      
+echo                                                                   -ohNMMMmmNds/:NMm.  -No+.:ohNMMMMMMmo-                                    
+echo                                                                     `.:/ohho+omMMMMd .NMMMMMNMMMMms/-`                                      
+echo                                                                          /dNho.ymMMm  dMsMMm+MMy-                                           
+echo                                                                           `yMs-MMMm.   .+hyhhy:                                             
+echo                                                                            `ym-MMm.` ` .mNhymo`                                             
+echo                                                                            hh:dMMd/y.:ddMMoN:N+                                             
+echo                                                                            Nm/MMMd+y`-MNsdNN.Ns                                             
+echo                                                                            +.+MNMsm+`-MNsdMM+s-                                             
+echo                                                                              :N-mMmm/-MMMmyMo`                                              
+echo                                                                              `o mMMM/`Nmmd`/`                                               
+echo                                                                                 yyMh` yh:y                                                  
+echo                                                                                 ``N+  oy `                                                  
+echo                                                                                   +.  .:                                                    
+title Welcome to My Territory                                                                                                                                               
+echo.
+REM [echo.] means print a blank line or it means a new line
+echo.
+echo                                                              W E L C O M E   T O   M Y   T E R R I T O R Y
+pause>nul
+:: pause>nul means do not print the line [Press any Key to Continue]
+REM [REM] & [::] both is used for Comment purpose 
+                                                                                                   
